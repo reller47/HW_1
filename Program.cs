cb21a94baf2b7c9bf43eff599f82cd7d48a7fe0f
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-08-08 13:34:19 +0300

    Task_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Ввудите число a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ввудите число b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a > b){
+    Console.WriteLine("\nMax = " + a);
+}
+else{
+    if (b > a){
+        Console.WriteLine("\nMax = " + b);
+    }
+    else{
+    if (a == b){
+        Console.WriteLine("\nЧисла равны");
+    }
+}
+}
+
+
+
